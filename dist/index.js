cfd88640303eaf7b97d90b376f78d3d3b271d31f
Jurgis Bridzius <jurgis@bridzius.lt>
2023-04-27 08:22:47 +0300

    working commit parsing

--- dist/index.js
+++ dist/index.js
@@ -4092,33 +4092,32 @@ const exec = __nccwpck_require__(514);
 
 try {
 	action();
-	core.setOutput('updates', "network");
 } catch (error) {
   	core.setFailed(error.message);
 }
 
 async function action() {
-  	const prefix = core.getInput('commit-prefix', { required: true });
-	console.log(prefix);
+  	const prefix = core.getInput('prefix', { required: true });
 	const raw_body = await readCommit();
 	const read_commit = raw_body.trimEnd();
-	console.log(`
-	Next line should be the commit
-	${read_commit}
-	This line is after the commit
-	`);
-	const matcher = new RegExp(`${prefix}.*`);
-	console.log(read_commit.match(matcher));
+	const matcher = new RegExp(`(?<=${prefix}).*`);
+	const post = read_commit.match(matcher);
+	if(post !== null) {
+		core.setOutput('post', post[0].trimStart());
+	}
 }
 
 async function readCommit() {
 	let log = '';
 	const listeners = {
 		stdout: (data) => {
-			log += data;
+			log += data.toString();
 		}
 	}
-	await exec.exec('git log -1 --no-merges --format="%b"', { listeners });
+	await exec.exec(
+		'git log', ['-1', '--no-merges', '--format="%b"'], 
+		{ listeners }
+	);
 	return log;
 }
 
